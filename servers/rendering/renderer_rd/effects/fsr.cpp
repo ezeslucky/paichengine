569e4d01f317ae2cbe8d5f14e0bae005f59bd566
Lucky <lucky123@gmail.com>
2025-08-22 23:07:24 +0530

    check solve

--- servers/rendering/renderer_rd/effects/fsr.cpp
+++ servers/rendering/renderer_rd/effects/fsr.cpp
@@ -2,11 +2,11 @@
 /*  fsr.cpp                                                               */
 /**************************************************************************/
 /*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
+/*                             PAICH ENGINE                             */
+/*                                                 */
 /**************************************************************************/
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
-/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*  Copyright (c) 2025-present Paich Engine contributors (see AUTHORS.md).  */
+/*                    */
 /*                                                                        */
 /* Permission is hereby granted, free of charge, to any person obtaining  */
 /* a copy of this software and associated documentation files (the        */
